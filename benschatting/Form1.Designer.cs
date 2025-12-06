940a5e14a2b103ca2abe0b268d0c9235ccb79a67
Ben harris <70765072+ben-har@users.noreply.github.com>
2025-12-06 13:13:39 -0600

    Add files via upload

---
+++ benschatting/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace benschatting
+{
+    partial class messaging_application
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.sa = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.label1.Location = new System.Drawing.Point(59, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 39);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "protos";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(70, 71);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(945, 336);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(70, 435);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(863, 22);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // sa
+            // 
+            this.sa.Location = new System.Drawing.Point(940, 433);
+            this.sa.Name = "sa";
+            this.sa.Size = new System.Drawing.Size(75, 23);
+            this.sa.TabIndex = 3;
+            this.sa.Text = "send";
+            this.sa.UseVisualStyleBackColor = true;
+            this.sa.Click += new System.EventHandler(this.sa_Click);
+            // 
+            // messaging_application
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Silver;
+            this.ClientSize = new System.Drawing.Size(1416, 661);
+            this.Controls.Add(this.sa);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Name = "messaging_application";
+            this.Text = "protos";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button sa;
+    }
+}
+
---
+++ benschatting/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Diagnostics;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace benschatting
+{
+    //client side is almost finished it just needs to take the message recieved from the server and put it in the rich text box
+    public partial class messaging_application : Form
+    {
+        string text = "string did not take in value";
+        TcpClient client = new TcpClient();
+       
+        public messaging_application()
+        {
+            InitializeComponent();
+            ReceiveMessage(client,richTextBox1);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+             
+
+        }
+
+        private void sa_Click(object sender, EventArgs e)
+        {
+
+            
+           
+
+            text = textBox1.Text;
+
+           
+
+            textBox1.Clear();
+
+            SendMessage(text, client);
+            richTextBox1.Text =  $"{richTextBox1.Text} \n {text
+                }";
+           
+        }
+
+        private void SendMessage(string str, TcpClient client)
+        {
+
+            
+
+            NetworkStream stream = client.GetStream();
+
+            byte[] message = Encoding.UTF8.GetBytes(str);
+
+            stream.Write(message, 0, message.Length);
+
+            Debug.WriteLine($"the debug before reset is {message.ToString()}");
+
+            message = new byte[1024];
+
+            Debug.WriteLine($"the debug after reset is {message.ToString()}");
+        }
+        private void ReceiveMessage(TcpClient client, RichTextBox textbox)
+        {
+            
+
+            client.Connect("127.0.0.1", 5000);
+            NetworkStream stream = client.GetStream();
+            byte[] buffer = new byte[1024];
+
+            Thread mythread = new Thread(() => {
+
+                while (true)
+                {
+                   
+
+                    int bytesread = stream.Read(buffer, 0, buffer.Length);
+
+                    if (bytesread < 0)
+                    {
+                        Debug.Write("server connection broken");
+                        break;
+                    }
+
+                    string returnmessage = Encoding.UTF8.GetString(buffer,0,bytesread);
+
+                    if(textbox.InvokeRequired)
+                    {
+                        textbox.Invoke((MethodInvoker)delegate
+                        {
+                            textbox.Text = $"{textbox.Text}  \n {returnmessage}";
+                        });
+
+                    }
+                    else
+                    {
+                        textbox.Text = $"{textbox.Text}  \n {returnmessage}";
+                    }
+
+                   buffer = new byte[1024];
+                }
+
+
+
+
+
+
+
+
+            });
+            mythread.Start();
+
+
+        }
+
+       
+    }
+}
